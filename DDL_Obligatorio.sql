467f05e4ff77a0ea0542f50dfab1eec5fc7cc866
Sebastian Piazza <sebastian.piazza@globant.com>
2021-09-14 21:30:20 -0300

    latest changes

--- DDL_Obligatorio.sql
+++ DDL_Obligatorio.sql
@@ -3,16 +3,10 @@ BEGIN
 CREATE DATABASE [ObligatorioBD2_GestionMetro];
 END;
 
-
-
 GO
-
-
 
 use [ObligatorioBD2_GestionMetro];
 GO
-
-
 
 /*
 -- if a Transact-SQL statement raises a run-time error,
@@ -20,26 +14,20 @@ the entire transaction is terminated and rolled back
 */
 Set xact_abort On
 
-
-
 Begin Transaction
 
-
-
 IF NOT EXISTS (SELECT * FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[ESTACION]'))
-Create Table [dbo].[ESTACION]
+WHERE object_id = OBJECT_ID(N'[dbo].[Estacion]'))
+Create Table [dbo].[Estacion]
 (
 [Codigo] INT Not Null Primary Key,
 [Barrio] VARCHAR(50) Not Null, --El barrio debe tener siempre un valor (nombre)
 [Descripcion] VARCHAR(150) Null,
 )
 
-
-
 IF NOT EXISTS (SELECT * FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[TREN]'))
-Create Table [dbo].[TREN]
+WHERE object_id = OBJECT_ID(N'[dbo].[Tren]'))
+Create Table [dbo].[Tren]
 (
 [Numero] INT Not Null Primary Key,
 [Descripcion] VARCHAR(150) Null,
@@ -50,8 +38,8 @@ Create Table [dbo].[TREN]
 
 
 IF NOT EXISTS (SELECT * FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[LINEA_METRO]'))
-Create Table [dbo].[LINEA_METRO]
+WHERE object_id = OBJECT_ID(N'[dbo].[LineaMetro]'))
+Create Table [dbo].[LineaMetro]
 (
 [Numero] INT Not Null Primary Key,
 [Descripcion] Varchar(150) Null,
@@ -64,10 +52,10 @@ Create Table [dbo].[LINEA_METRO]
 
 
 IF NOT EXISTS (SELECT * FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[ESTACION_LINEA_METRO]'))
-Create Table [dbo].[ESTACION_LINEA_METRO]
+WHERE object_id = OBJECT_ID(N'[dbo].[EstacionLineaMetro]'))
+Create Table [dbo].[EstacionLineaMetro]
 (
-[CodigoEstacion] INT Not Null,
+[CodigoEstacion] INT Not Null ,
 [NumeroLinea] INT Not Null,
 Primary Key(CodigoEstacion, NumeroLinea)
 )
@@ -75,8 +63,8 @@ Primary Key(CodigoEstacion, NumeroLinea)
 
 
 IF NOT EXISTS (SELECT * FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[TREN_ESTACION_LINEA_METRO]'))
-Create Table [dbo].[TREN_ESTACION_LINEA_METRO]
+WHERE object_id = OBJECT_ID(N'[dbo].[TrenEstacionLineaMetro]'))
+Create Table [dbo].[TrenEstacionLineaMetro]
 (
 [CodigoEstacion] INT Not Null,
 [NumeroLinea] INT Not Null,
@@ -85,85 +73,59 @@ Create Table [dbo].[TREN_ESTACION_LINEA_METRO]
 [Hora] Time Not Null,
 Primary Key(CodigoEstacion,NumeroLinea,NumeroTren)
 )
-
-Commit Transaction
-
-GO
 
 --Restriciones 
 --Estacion
 
 /*
-IF NOT EXISTS (SELECT * FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[ESTACION]'))
-Create Table [dbo].[ESTACION]
-(
-[Codigo] INT Not Null Primary Key,
-[Barrio] VARCHAR(50) Not Null, --El barrio debe tener siempre un valor (nombre)
-[Descripcion] VARCHAR(150) Null,
-)
+constraints Tren
 */
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'UK_TrenLetra'))
+ALTER TABLE Tren ADD CONSTRAINT UK_TrenLetra UNIQUE(Letra)
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'CK_TrenCapVag'))
+ALTER TABLE Tren ADD CONSTRAINT CK_TrenCapVag CHECK (CapacidadVagon <= 40)
 
 /*
-IF NOT EXISTS (SELECT * FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[TREN]'))
-Create Table [dbo].[TREN]
-(
-[Numero] INT Not Null Primary Key,
-[Descripcion] VARCHAR(150) Null,
-[Letra] CHAR(1) not null,
-[CantidadVagon] INT not null, 
-[CapacidadVagon] INT not null, --La capacidad debe ser menor o igual 40 asientos 
-)
+constraints LineaMetro
 */
-ALTER TABLE TREN ADD CONSTRAINT UK_TrenLetra UNIQUE(Letra)
-ALTER TABLE TREN ADD CONSTRAINT CK_TrenCapVag CHECK (CapacidadVagon <= 40)
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'CK_LineaMetro_Longitud'))
+ALTER TABLE LineaMetro ADD CONSTRAINT CK_LineaMetro_Longitud CHECK(Longitud >= 1)
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'CK_LineaMetro_CodColor'))
+ALTER TABLE LineaMetro ADD CONSTRAINT CK_LineaMetro_CodColor CHECK(CodigoColor IN ('ROJO', 'AZUL', 'AMARILLO', 'NARANJA', 'VERDE','NEGRO'))
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'FK_LineaMetro_Inicial'))
+ALTER TABLE LineaMetro ADD CONSTRAINT FK_LineaMetro_Inicial FOREIGN KEY(CodigoEstacionInicial) REFERENCES Estacion(Codigo)
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'FK_LineaMetro_Final'))
+ALTER TABLE LineaMetro ADD CONSTRAINT FK_LineaMetro_Final FOREIGN KEY(CodigoEstacionFinal) REFERENCES Estacion(Codigo)
 
 /*
-IF NOT EXISTS (SELECT * FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[LINEA_METRO]'))
-Create Table [dbo].[LINEA_METRO]
-(
-[Numero] INT Not Null Primary Key,
-[Descripcion] Varchar(150) Null,
-[Longuitud] DECIMAL(10,3) Not Null,
-[CodigoColor] Varchar(50) Not Null,
-[CodigoEstacionInicial] INT Not Null,
-[CodigoEstacionFinal] INT Not Null,
-)
-
-Rojo, Azul, Amarillo, Naranja, Verde y Negro.
+constraints EstacionLineaMetro
 */
-ALTER TABLE LINEA_METRO ADD CONSTRAINT CK_Linea_Metro_Longitud CHECK(Longitud >= 1)
-ALTER TABLE LINEA_METRO ADD CONSTRAINT CK_Linea_Metro_CodColor CHECK(CodigoColor IN ('ROJO', 'AZUL', 'AMARILLO', 'NARANJA', 'VERDE','NEGRO'))
-ALTER TABLE LINEA_METRO ADD CONSTRAINT FK_Linea_Metro_Inicial FOREIGN KEY(CodigoEstacionInicial) REFERENCES ESTACION(Codigo)
-ALTER TABLE LINEA_METRO ADD CONSTRAINT FK_Linea_Metro_Final FOREIGN KEY(CodigoEstacionFinal) REFERENCES ESTACION(Codigo)
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'FK_EstacionLineaMetro_CodigoEstacion'))
+ALTER TABLE EstacionLineaMetro ADD CONSTRAINT FK_EstacionLineaMetro_CodigoEstacion FOREIGN KEY(CodigoEstacion) REFERENCES Estacion(Codigo)
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'FK_EstacionLineaMetro_NumeroLinea'))
+ALTER TABLE EstacionLineaMetro ADD CONSTRAINT FK_EstacionLineaMetro_NumeroLinea FOREIGN KEY(NumeroLinea) REFERENCES LineaMetro(Numero)
 
 /*
-IF NOT EXISTS (SELECT * FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[ESTACION_LINEA_METRO]'))
-Create Table [dbo].[ESTACION_LINEA_METRO]
-(
-[CodigoEstacion] INT Not Null,
-[NumeroLinea] INT Not Null,
-Primary Key(CodigoEstacion, NumeroLinea)
-)
+TrenEstacionLineaMetro
 */
-ALTER TABLE ESTACION_LINEA_METRO ADD CONSTRAINT FK_ESTACION_LINEA_METRO_CodigoEstacion FOREIGN KEY(CodigoEstacion) REFERENCES ESTACION(Codigo)
-ALTER TABLE ESTACION_LINEA_METRO ADD CONSTRAINT FK_ESTACION_LINEA_METRO_NumeroLinea FOREIGN KEY(NumeroLinea) REFERENCES LINEA_METRO(Numero)
 
-/*
-IF NOT EXISTS (SELECT * FROM sys.objects
-WHERE object_id = OBJECT_ID(N'[dbo].[TREN_ESTACION_LINEA_METRO]'))
-Create Table [dbo].[TREN_ESTACION_LINEA_METRO]
-(
-[CodigoEstacion] INT Not Null,
-[NumeroLinea] INT Not Null,
-[NumeroTren] INT Not Null,
-[Fecha] Date Not Null,
-[Hora] Time Not Null,
-Primary Key(CodigoEstacion,NumeroLinea,NumeroTren)
-)
-*/
-ALTER TABLE TREN_ESTACION_LINEA_METRO ADD CONSTRAINT FK_TREN_ESTACION_LINEA_METRO_Linea_Metro FOREIGN KEY(CodigoEstacion, NumeroLinea) REFERENCES ESTACION_LINEA_METRO(CodigoEstacion, NumeroLinea)
-ALTER TABLE TREN_ESTACION_LINEA_METRO ADD CONSTRAINT FK_ESTACION_LINEA_METRO_Tren FOREIGN KEY(NumeroTren) REFERENCES TREN(Numero)
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'FK_TrenEstacionLineaMetro_Tren'))
+ALTER TABLE TrenEstacionLineaMetro ADD CONSTRAINT FK_TrenEstacionLineaMetro_Tren FOREIGN KEY(NumeroTren) REFERENCES Tren(Numero)
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'FK_TrenEstacionLineaMetro_Estacion'))
+ALTER TABLE TrenEstacionLineaMetro ADD CONSTRAINT FK_TrenEstacionLineaMetro_Estacion FOREIGN KEY(CodigoEstacion) REFERENCES Estacion(Codigo)
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'FK_TrenEstacionLineaMetro_LineaMetro'))
+ALTER TABLE TrenEstacionLineaMetro ADD CONSTRAINT FK_TrenEstacionLineaMetro_LineaMetro FOREIGN KEY(NumeroLinea) REFERENCES LineaMetro(Numero)
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'FK_TrenEstacionLineaMetro_EstacionLineaMetro'))
+ALTER TABLE TrenEstacionLineaMetro ADD CONSTRAINT FK_TrenEstacionLineaMetro_EstacionLineaMetro FOREIGN KEY(CodigoEstacion, NumeroLinea) REFERENCES EstacionLineaMetro(CodigoEstacion, NumeroLinea)
+
+
+Commit Transaction
