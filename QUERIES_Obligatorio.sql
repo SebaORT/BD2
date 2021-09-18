4694e6c10c6e90ce0eb1353f9a8115da98c65f63
ceciliaB97 <cecilia.belon@hotmail.com>
2021-09-18 15:12:00 -0300

    Create QUERIES_Obligatorio.sql

---
+++ QUERIES_Obligatorio.sql
@@ -0,0 +1,6 @@
+USE [ObligatorioBD2_GestionMetro]
+GO
+--a.Mostrar los datos del/los trene/s que pasaron por última vez por una línea-estación
+SELECT * FROM EstacionLineaMetro;
+
+--b.Mostrar los datos de las estaciones por las que pasaron más trenes este año que la cantidad promedio de trenes que pasaron en el año anterior.
