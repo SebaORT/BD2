5b729b53c4b2f1a438efa279f56538fb937eb8ff
ceciliaB97 <cecilia.belon@hotmail.com>
2021-09-19 17:57:22 -0300

    CAMBIOS

--- QUERIES_Obligatorio.sql
+++ QUERIES_Obligatorio.sql
@@ -4,3 +4,17 @@ GO
 SELECT * FROM EstacionLineaMetro;
 
 --b.Mostrar los datos de las estaciones por las que pasaron más trenes este año que la cantidad promedio de trenes que pasaron en el año anterior.
+SELECT *
+FROM Estacion e1
+WHERE e1.Codigo IN (
+	SELECT tel.codigoEstacion, tel.NumeroTren, SUM(tel.numeroTren), AVG(tel.NumeroTren) AS PromedioAnioAnterior
+	FROM TrenEstacionLineaMetro tel
+	WHERE AVG(tel.NumeroTren) = (
+			SELECT AVG(tel.NumeroTren)
+			FROM TrenEstacionLineaMetro tel
+			GROUP BY tel.codigoEstacion, tel.NumeroTren
+			HAVING (tel.Fecha BETWEEN DAY('2020-01-01') AND DAY('2020-12-31'))
+		)
+	GROUP BY tel.codigoEstacion, tel.NumeroTren
+	HAVING (tel.Fecha BETWEEN DAY('2021-01-01') AND GETDATE())
+	)
